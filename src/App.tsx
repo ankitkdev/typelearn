fbab496fd26958059af767cfc08c8bb08b72fe21
ankitkdev <ankitrajpoot005@gmail.com>
2023-05-09 11:46:37 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,25 @@ export interface IProducts {
   price: number;
 }
 
+export interface IMagic {
+  id: number;
+  active: boolean;
+}
+
 function App() {
   const [products, setProducts] = useState<IProducts[]>([{ name: "iphone", price: 7899 }, { name: "macbook", price: 9000 }])
+  const myData : IMagic ={
+    id: 344,
+    active: true,
+  }
+
+  function handleClick (x: string) : void  {
+    console.log(x)
+  }
   return (
     <>
-      {products.map((product)=>{
-        return <Product product={product} key={product.name}/>
+      {products.map((product) => {
+        return <Product product={product} myData={myData} key={product.name} handleClick={handleClick} />
       })}
     </>
   );
--- src/Product.tsx
+++ src/Product.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
 import { IProducts } from './App'
+import {IMagic} from './App';
 
 interface ProductProps {
     product: IProducts;
+    myData: IMagic
+    handleClick(name: string): void; 
 }
 
-function Product({product}: ProductProps) {
-  
+function Product({product, myData, handleClick}: ProductProps) {
   return (
     <div>
       {product.name}
+      {product.price}
+      {myData.id}
+      <button onClick={()=>handleClick(product.name)}>click me</button>
     </div>
   );
 }
